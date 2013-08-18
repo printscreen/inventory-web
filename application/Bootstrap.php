555adb1a5f6cbcf4b57dcfdf284e78d3cc3158ac
Britton Mathews <brittonmathews@gmail.com>
2013-08-17 21:03:31 -0700

    Base files

---
+++ application/Bootstrap.php
@@ -0,0 +1,48 @@
+<?php
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+	protected function _initDefines()
+	{
+		defined('SESSION') || define('SESSION', 'User_Session');
+	    defined('TOKEN') || define('TOKEN', 'User_Token');
+	}
+	
+	protected function _initAutoload()
+	{
+		$autoLoader = Zend_Loader_Autoloader::getInstance();
+		$autoLoader->registerNamespace('Inventory_');
+		$resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+                            'basePath'        => APPLICATION_PATH,
+                            'namespace'     => '',
+                            'resourceTypes'    => array(
+                                                'form'    => array(
+                                                            'path'        => '/modules/default/views/forms/',
+                                                            'namespace'    => 'Form_'
+                                                        ),
+                                                'model'    => array(
+                                                            'path'        => '/models/',
+                                                            'namespace'    => 'Model_'
+                                                        )
+                                                )
+                            ));
+		return $autoLoader;
+	}
+	
+	protected function _initApplication()
+	{
+	    date_default_timezone_set($this->getOption('default_time_zone'));
+	}
+
+    protected function _initSession()
+    {
+        $session = new Zend_Session_Namespace(SESSION);
+        Zend_Registry::set(SESSION, $session);
+    }
+	
+    protected function _initPlugins()
+    {
+        $frontController = Zend_Controller_Front::getInstance();
+        $frontController->registerPlugin(new Inventory_Controller_Plugin_Init());
+        $frontController->registerPlugin(new Inventory_Controller_Plugin_Acl());
+    }
+}
---
+++ library/Inventory/Controller/Plugin/Acl.php
@@ -0,0 +1,57 @@
+<?php
+class Inventory_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
+{
+	protected $_publicModules;
+	protected $_publicControllers;
+	protected $_publicActions;
+	
+	public function __construct()
+	{
+		$this->_publicModules = array();
+		$this->_publicControllers = array('default:error');
+		$this->_publicActions = array(
+				  'default:auth:login'
+				, 'default:auth:forgot-password'
+				, 'default:auth:reset-password'
+				, 'default:auth:logout'
+				, 'default:error:error');
+	}
+	
+    public function dispatchLoopStartup(Zend_Controller_Request_Abstract $request)
+	{
+	    //If not dispatchable
+		if(!(Zend_Controller_Front::getInstance()->getDispatcher()->isDispatchable($request))) {
+            return false;
+        }
+	    
+	    $reqModule = $this->getRequest()->getModuleName();  
+		$reqController = $this->getRequest()->getControllerName();
+		$reqAction = $this->getRequest()->getActionName();
+		$reqModuleStr = $reqModule;
+		$reqControllerStr = $reqModule.':'.$reqController;
+		$reqActionStr = $reqModule.':'.$reqController.':'.$reqAction;
+
+		if( in_array($reqControllerStr, $this->_publicControllers) ||
+			in_array($reqActionStr, $this->_publicActions)) {
+			//If module, controller, or action is publically open, don't run it through ACL
+			return;
+		}
+
+	    $r = Zend_Controller_Action_HelperBroker::getStaticHelper('redirector');    
+		if(!Zend_Auth::getInstance()->hasIdentity() || empty(Zend_Registry::get(SESSION)->token)) {			
+    		if($request->isXMLHttpRequest()) {
+    		    header('Cache-Control: no-cache, must-revalidate');
+                header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+                header('Content-type: text/json');
+                echo json_encode(array(
+                	'sessionExpired'=>true, 
+                	'url' => '/auth/login',
+                    'message' => 'Your session has expired. Redirecting to the Login page.'
+                ));     
+    		} else {
+    		    $r->gotoUrl('/auth/login')->redirectAndExist();
+    		}
+		    exit;
+		}
+	}
+}
