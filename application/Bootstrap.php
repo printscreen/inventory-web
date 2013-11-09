13ec401c251ded85dd19dafee15f2bc4f00ef729
Britton Mathews <brittonmathews@gmail.com>
2013-11-08 16:35:16 -0800

    Fixed spaces

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -13,19 +13,19 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $autoLoader = Zend_Loader_Autoloader::getInstance();
         $autoLoader->registerNamespace('Inventory_');
         $resourceLoader = new Zend_Loader_Autoloader_Resource(array(
-                            'basePath'        => APPLICATION_PATH,
-                            'namespace'     => '',
-                            'resourceTypes'    => array(
-                                                'form'    => array(
-                                                            'path'        => '/modules/default/views/forms/',
-                                                            'namespace'    => 'Form_'
-                                                        ),
-                                                'model'    => array(
-                                                            'path'        => '/models/',
-                                                            'namespace'    => 'Model_'
-                                                        )
-                                                )
-                            ));
+                            'basePath'	=> APPLICATION_PATH,
+                            'namespace'	=> '',
+                            'resourceTypes'	=> array(
+                            	'form'    => array(
+                                	'path'        => '/modules/default/views/forms/',
+                                    'namespace'    => 'Form_'
+                                ),
+                                'model'    => array(
+                                	'path'        => '/models/',
+                                    'namespace'    => 'Model_'
+                                )
+                            )
+                        ));
         return $autoLoader;
     }
 
