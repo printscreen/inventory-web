9b0742d7d211d0b920cc13e8f818c965c15c7172
Britton Mathews <brittonmathews@gmail.com>
2013-12-29 09:21:30 -0800

    Cleaned up login screen

--- application/modules/default/views/forms/Login.php
+++ application/modules/default/views/forms/Login.php
@@ -4,33 +4,33 @@ class Form_Login extends Inventory_Form
     public function __construct($options = null)
     {
         parent::__construct($options);
-        
+
         $email = new Zend_Form_Element_Text('email');
         $email->setRequired(true)
-              ->addValidator('NotEmpty', true)
-              ->addFilter('StripTags')
-              ->addFilter('StringTrim')
-              ->addFilter('StringToLower')
-              ->addValidator('NotEmpty',true)
-              ->addValidator('EmailAddress')
-              ->addErrorMessage('Must be a valid email address')
-              ->setAttrib('class', 'input-block-level')
-              ->setAttrib('placeholder', 'Email address')
-              ->removeDecorator('HtmlTag')
-              ->removeDecorator('Label');
+            ->addValidator('NotEmpty', true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addFilter('StringToLower')
+            ->addValidator('NotEmpty',true)
+            ->addValidator('EmailAddress')
+            ->addErrorMessage('Must be a valid email address')
+            ->setAttrib('class', 'form-control')
+            ->setAttrib('placeholder', 'Email address')
+            ->removeDecorator('HtmlTag')
+            ->removeDecorator('Label');
         $this->addElement($email);
-                
+
         $password = new Zend_Form_Element_Password('password');
-    	$password->setRequired(true)
-    	          ->addValidator('NotEmpty', true)
-              	  ->addFilter('StripTags')
-              	  ->addFilter('StringTrim')
-              	  ->addValidator('NotEmpty', true)
-              	  ->addErrorMessage('Password required')
-              	  ->setAttrib('class', 'input-block-level')
-              	  ->setAttrib('placeholder', 'Password')
-              	  ->removeDecorator('HtmlTag')
-              	  ->removeDecorator('Label');
+        $password->setRequired(true)
+            ->addValidator('NotEmpty', true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty', true)
+            ->addErrorMessage('Password required')
+            ->setAttrib('class', 'form-control')
+            ->setAttrib('placeholder', 'Password')
+            ->removeDecorator('HtmlTag')
+            ->removeDecorator('Label');
         $this->addElement($password);
     }
 }
